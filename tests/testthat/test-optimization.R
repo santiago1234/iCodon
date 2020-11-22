067eaca219239715285c129c5f3f5b92b3a4bfb4
Santiago Medina <santiagogmm@hotmail.com>
2020-11-22 15:58:34 -0600

    pacakge vignette

--- tests/testthat/test-optimization.R
+++ tests/testthat/test-optimization.R
@@ -5,7 +5,7 @@ results <- optimizer(seq_to_test, specie = "mouse", n_iterations = 3)
 results2 <- optimizer(seq_to_test, specie = "mouse", n_iterations = 3)
 
 test_that("optimized sequence is more stable than input sequence", {
-  expect_true(results$half_life[1] < results$half_life[3])
+  expect_true(results$predicted_stability[1] <= results$predicted_stability[3])
 })
 
 test_that("results are reproducible by using the seed", {
